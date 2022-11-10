0203bdb2bb3426983bf08b58203744d2d392017b
Alvin Wanjohi <akwanjohi99@gmail.com>
2022-11-10 17:25:15 +0300

    Add function to check login status

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@ function setSession($id, $username, $user_type)
     $_SESSION['username'] = $username;
     $_SESSION['user_type'] = $user_type;
 }
+
+function checkIfLoggedIn()
+{
+    if (isset($_SESSION['user_id'])) {
+        header('Location: /');
+        exit;
+    }
+}
